30d22850dff15c25a46607e86c13560ac7a08ef1
Eduardo Leão Garcia <edulg72@gmail.com>
2017-12-02 18:50:28 -0200

    Initial deploy

---
+++ db/migrate/20171202203825_create_identities.rb
@@ -0,0 +1,11 @@
+class CreateIdentities < ActiveRecord::Migration
+  def change
+    create_table :identities do |t|
+      t.string :name
+      t.string :email
+      t.string :phone
+
+      t.timestamps null: false
+    end
+  end
+end
